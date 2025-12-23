6bcf3042a6e49e09558a41c67cf9ce6635423d1f
kca2250 <2250katok@gmail.com>
2025-12-23 22:17:19 +0900

    create init.lua

---
+++ nvim/init.lua
@@ -0,0 +1,20 @@
+-- lazy.nvim ブートストラップ
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- 基本設定
+require("config.options")
+require("config.keymaps")
+
+-- lazy.nvim 起動
+require("lazy").setup("plugins")

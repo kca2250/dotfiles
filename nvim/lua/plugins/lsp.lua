15c36aaf16b7ac86101a716815ee7e5dc5b8c411
kca2250 <2250katok@gmail.com>
2025-12-23 23:30:00 +0900

    modify lsp settings

---
+++ nvim/lua/plugins/lsp.lua
@@ -0,0 +1,44 @@
+return {
+	-- nvim-lspconfig: サーバー設定を提供
+	{ "neovim/nvim-lspconfig" },
+
+	-- Mason: LSPサーバー管理
+	{
+		"mason-org/mason.nvim",
+		opts = {},
+	},
+
+	-- Mason-LSPConfig: 連携
+	{
+		"mason-org/mason-lspconfig.nvim",
+		dependencies = {
+			"mason-org/mason.nvim",
+			"neovim/nvim-lspconfig",
+		},
+		opts = {
+			ensure_installed = {
+				"pyright",
+				"ts_ls",
+				"html",
+				"cssls",
+				"lua_ls",
+				"ruby_lsp",
+			},
+			automatic_enable = true,
+		},
+	},
+
+	-- Mason Tool Installer: formatter/linter管理
+	{
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		dependencies = { "mason-org/mason.nvim" },
+		opts = {
+			ensure_installed = {
+				"black",
+				"prettier",
+				"stylua",
+				"rubocop",
+			},
+		},
+	},
+}

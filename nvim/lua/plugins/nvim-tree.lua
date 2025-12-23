6913309dfdf0975baf5666148fbad1542b5ecdef
kca2250 <2250katok@gmail.com>
2025-12-23 22:39:46 +0900

    add nvim-tree

---
+++ nvim/lua/plugins/nvim-tree.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "nvim-tree/nvim-tree.lua",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    keys = {
+      { "<leader>e", "<cmd>NvimTreeToggle<cr>", desc = "Toggle file tree" },
+    },
+    config = function()
+      require("nvim-tree").setup({
+        view = {
+          width = 30,
+        },
+      })
+    end,
+  },
+}

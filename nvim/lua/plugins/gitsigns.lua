68c764405316a34530e81403da401fac59a14437
kca2250 <2250katok@gmail.com>
2025-12-23 22:40:38 +0900

    add gitsigns

---
+++ nvim/lua/plugins/gitsigns.lua
@@ -0,0 +1,32 @@
+return {
+  {
+    "lewis6991/gitsigns.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+      require("gitsigns").setup({
+        signs = {
+          add = { text = "│" },
+          change = { text = "│" },
+          delete = { text = "_" },
+          topdelete = { text = "‾" },
+          changedelete = { text = "~" },
+        },
+        current_line_blame = false,
+        on_attach = function(bufnr)
+          local gs = package.loaded.gitsigns
+          local keymap = vim.keymap.set
+
+          -- ナビゲーション
+          keymap("n", "]h", gs.next_hunk, { buffer = bufnr, desc = "Next hunk" })
+          keymap("n", "[h", gs.prev_hunk, { buffer = bufnr, desc = "Prev hunk" })
+
+          -- アクション
+          keymap("n", "<leader>hs", gs.stage_hunk, { buffer = bufnr, desc = "Stage hunk" })
+          keymap("n", "<leader>hr", gs.reset_hunk, { buffer = bufnr, desc = "Reset hunk" })
+          keymap("n", "<leader>hp", gs.preview_hunk, { buffer = bufnr, desc = "Preview hunk" })
+          keymap("n", "<leader>hb", gs.blame_line, { buffer = bufnr, desc = "Blame line" })
+        end,
+      })
+    end,
+  },
+}

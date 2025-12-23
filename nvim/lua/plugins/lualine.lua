8482c7f3afc5e5ea1172b06ecb2473870b755595
kca2250 <2250katok@gmail.com>
2025-12-24 08:58:25 +0900

    change display file path

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,13 +1,18 @@
 return {
-  {
-    "nvim-lualine/lualine.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function()
-      require("lualine").setup({
-        options = {
-          theme = "catppuccin",
-        },
-      })
-    end,
-  },
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			require("lualine").setup({
+				options = {
+					theme = "catppuccin",
+				},
+				sections = {
+					lualine_c = {
+						{ "filename", path = 3 },
+					},
+				},
+			})
+		end,
+	},
 }

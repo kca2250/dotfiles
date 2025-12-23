5303490ac0832f42d5cc9bbb6331c6bd09735f0d
kca2250 <2250katok@gmail.com>
2025-12-23 23:07:10 +0900

    formatter

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -1,16 +1,16 @@
 return {
-  {
-    "nvim-tree/nvim-tree.lua",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    keys = {
-      { "<leader>e", "<cmd>NvimTreeToggle<cr>", desc = "Toggle file tree" },
-    },
-    config = function()
-      require("nvim-tree").setup({
-        view = {
-          width = 30,
-        },
-      })
-    end,
-  },
+	{
+		"nvim-tree/nvim-tree.lua",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		keys = {
+			{ "<leader>e", "<cmd>NvimTreeToggle<cr>", desc = "Toggle file tree" },
+		},
+		config = function()
+			require("nvim-tree").setup({
+				view = {
+					width = 30,
+				},
+			})
+		end,
+	},
 }

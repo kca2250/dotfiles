2b0f3465b91f1a6f59e8a91c85b87b42fd6490b1
kca2250 <2250katok@gmail.com>
2025-12-24 15:35:36 +0900

    window分割用のキーバインドを追加

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -22,3 +22,9 @@ keymap("n", "<A-Up>", ":resize +2<CR>", { desc = "Height +2" })
 keymap("n", "<A-Down>", ":resize -2<CR>", { desc = "Height -2" })
 keymap("n", "<A-Left>", ":vertical resize -2<CR>", { desc = "Width -2" })
 keymap("n", "<A-Right>", ":vertical resize +2<CR>", { desc = "Width +2" })
+
+-- ウィンドウ分割
+keymap("n", "<leader>sv", ":vs<CR>", { desc = "Split vertical" })
+keymap("n", "<leader>sh", ":sp<CR>", { desc = "Split horizontal" })
+keymap("n", "<leader>sx", ":close<CR>", { desc = "Close split" })
+keymap("n", "<leader>se", "<C-w>=", { desc = "Equal size splits" })

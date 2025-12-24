62b154e1c20142b42f9bbf33b352ab33d4337f0a
kca2250 <2250katok@gmail.com>
2025-12-24 09:00:03 +0900

    add toggleterm

---
+++ nvim/lua/plugins/toggleterm.lua
@@ -0,0 +1,28 @@
+return {
+	{
+		"akinsho/toggleterm.nvim",
+		version = "*",
+		keys = {
+			{ "<C-\\>", "<cmd>ToggleTerm<cr>", desc = "Toggle terminal" },
+		},
+		config = function()
+			require("toggleterm").setup({
+				size = 15,
+				direction = "horizontal",
+				shade_terminals = false,
+			})
+
+			-- ターミナル内でのキーマップ
+			function _G.set_terminal_keymaps()
+				local opts = { buffer = 0 }
+				vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
+				vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
+				vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
+				vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
+				vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]], opts)
+			end
+
+			vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
+		end,
+	},
+}

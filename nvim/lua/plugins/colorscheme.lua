0a6c90cb012beddd730e73b80598616fa63551c8
kca2250 <2250katok@gmail.com>
2026-01-04 11:12:38 +0900

    背景透過設定を追加

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,13 +1,19 @@
 return {
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-    config = function()
-      require("catppuccin").setup({
-        flavour = "mocha",
-      })
-      vim.cmd.colorscheme("catppuccin")
-    end,
-  },
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		config = function()
+			require("catppuccin").setup({
+				flavour = "mocha",
+			})
+			vim.cmd.colorscheme("catppuccin")
+			-- 背景透過
+			vim.api.nvim_set_hl(0, "Normal", { bg = "NONE" })
+			vim.api.nvim_set_hl(0, "NormalFloat", { bg = "NONE" })
+			vim.api.nvim_set_hl(0, "NormalNC", { bg = "NONE" }) -- 非アクティブウィンドウも
+			vim.api.nvim_set_hl(0, "SignColumn", { bg = "NONE" })
+			vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "NONE" })
+		end,
+	},
 }

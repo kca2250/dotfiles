39cc09b9be2e34d2f2b00747e483023179f70e04
kca2250 <2250katok@gmail.com>
2026-02-16 22:27:43 +0900

    chore: sort高速化

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -2,7 +2,16 @@ return {
   {
     "nvim-telescope/telescope.nvim",
     branch = "master",
-    dependencies = { "nvim-lua/plenary.nvim" },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build = "make",
+        cond = function()
+          return vim.fn.executable("make") == 1
+        end,
+      },
+    },
     keys = {
       { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find files" },
       { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Live grep" },
@@ -14,7 +23,17 @@ return {
         defaults = {
           preview = false,
         },
+        extensions = {
+          fzf = {
+            fuzzy = true,
+            override_generic_sorter = true,
+            override_file_sorter = true,
+            case_mode = "smart_case",
+          },
+        },
       })
+
+      pcall(require("telescope").load_extension, "fzf")
     end,
   },
 }

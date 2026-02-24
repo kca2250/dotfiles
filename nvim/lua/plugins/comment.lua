87b9fd27f8da0efb55ebe1fe399552f144981e23
kca2250 <2250katok@gmail.com>
2026-02-24 12:38:49 +0900

    feat: typescriptファイル内いい感じにコメントできるように調整

--- nvim/lua/plugins/comment.lua
+++ nvim/lua/plugins/comment.lua
@@ -1,9 +1,15 @@
 return {
 	{
+		"JoosepAlviste/nvim-ts-context-commentstring",
+		lazy = true,
+	},
+	{
 		"numToStr/Comment.nvim",
 		event = "VeryLazy",
 		config = function()
-			require("Comment").setup()
+			require("Comment").setup({
+				pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+			})
 		end,
 	},
 }
